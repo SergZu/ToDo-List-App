fa4a6077669ad4d6e58064800a9d9e3a4fa428e2
SergZu <sergzu0@gmail.com>
2021-01-28 18:24:27 +0600

    Add types.ts

---
+++ src/components/types.ts
@@ -0,0 +1,20 @@
+type mark = 'none' | 'blue' | 'green' | 'red' | 'black';
+
+interface options {
+    showCompleted: boolean;
+    splited : boolean;
+    currentMark : mark;
+}
+
+export interface task {
+    id : number;
+    text : string;
+    complete : boolean;
+    mark : mark;
+}
+
+
+export interface TaskListProps {
+    tasks : task[];
+    viewOptions : options;
+} 
