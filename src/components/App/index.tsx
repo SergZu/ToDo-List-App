a1fe9c0e5fa891a53fa7e1c72c3a502b79f8452c
SergZu <sergzu0@gmail.com>
2021-01-28 23:21:15 +0600

    Added mock data

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -6,7 +6,9 @@ import { options, taskType } from '../types';
 const App = function() {
     const [data, setData] = useState( () : taskType[] =>([
         {text : 'Do anything', id : '1', complete : false, mark : 'none'},
-        {text : 'Do anything else', id : '2', complete : false, mark : 'none'}
+        {text : 'Do anything else', id : '2', complete : true, mark : 'none'},
+        {text : 'You spin me ...', id : '3', complete : false, mark : 'none'},
+        {text : 'Right now, baby right now', id : '4', complete : true, mark : 'none'},
     ]) );
     const getOptions = () : options => ({
         showCompleted: false,
@@ -14,7 +16,7 @@ const App = function() {
         currentMark : 'none',})
     const taskData = data;
 
-    const setTaskChecked = (id : string) : void => {
+    const setTaskChecked = (id : string) => {
         const taskData = [...data];
         const targetElement = taskData.find( (item) => item.id === id );
         if (targetElement !== undefined) targetElement.complete = !targetElement.complete; 
