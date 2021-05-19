5f91fa598c2e1336c1b5b4db165e6cb7ace236bf
SergZu <sergzu0@gmail.com>
2021-05-19 20:50:52 +0600

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import App from './components/App';
 window.addEventListener('load', async () => {
     if ('serviceWorker' in navigator) {
         try {
-            await navigator.serviceWorker.register('/serviceWorker.js')
+            await navigator.serviceWorker.register('/service-worker.js')
         }
         catch (err) {
             console.log('Service worker register failed')
