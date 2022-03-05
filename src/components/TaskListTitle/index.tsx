dd0e55547ee4ac09098186b867b301c3db57eb58
SergZu <sergzu0@gmail.com>
2022-03-05 18:21:16 +0600

    TaskListTitle component code refactoring

--- src/components/TaskListTitle/index.tsx
+++ src/components/TaskListTitle/index.tsx
@@ -4,35 +4,75 @@ import { TaskListTitleProps } from '../types';
 import {formatDate} from '../../dateUtils';
 import './styles.scss';
 
-const TaskListTitle = function({ currentFilter, currentDate, addTask, searchQuery, makeQuery, nextId } : TaskListTitleProps) {
+const TaskListTitle = function({ 
+                                currentFilter, 
+                                currentDate, 
+                                addTask, 
+                                searchQuery, 
+                                makeQuery, 
+                                nextId 
+                                } : TaskListTitleProps) {
+    
     const [ addTaskMode, switchAddTaskMode ] = useState(false);
     const toggleMode = () => {
         switchAddTaskMode( (modeState) => !modeState );
     }
+    
     const onSearchChangeHandler = (evt: React.ChangeEvent<HTMLInputElement>) => {
         makeQuery(evt.currentTarget.value);
     };
+    
     const onAddTaskClickHandler : React.MouseEventHandler = () => {
         toggleMode();
     };
+    
     const onAddTaskKeyPressedHandler : React.KeyboardEventHandler = (evt) => {
         if (evt.key === 'Enter') toggleMode();
     };
 
-    const filterName = currentFilter !== 'All' ? currentFilter : 'My Tasks';
+    const filterName = currentFilter !== 'All' ? 
+                                                  currentFilter : 
+                                                  'My Tasks';
     const formatedDate = formatDate(currentDate);
-    const listTitleClass = addTaskMode ? 'TaskListTitle open' : 'TaskListTitle';
-    const restLayout = addTaskMode ? (<TaskAddForm nextId={nextId} addTask={addTask} toggleMode={toggleMode} currentDate={currentDate} />) 
-        : (<><input className='TaskListTitle-search' type='text' value={searchQuery}
-        onChange={onSearchChangeHandler} placeholder={`Find in ${filterName}`} />
-        <button className='TaskListTitle-add' onClick={onAddTaskClickHandler} onKeyPress={onAddTaskKeyPressedHandler}>Add Task</button></>); 
+    const listTitleClass = addTaskMode ? 
+                                          'TaskListTitle open' : 
+                                          'TaskListTitle';
+    const restLayout = addTaskMode ? 
+                                    (<TaskAddForm 
+                                                    nextId={nextId} 
+                                                    addTask={addTask} 
+                                                    toggleMode={toggleMode} 
+                                                    currentDate={currentDate} 
+                                     />): 
+                                    (<>
+                                            <input 
+                                                    className='TaskListTitle-search' 
+                                                    type='text' 
+                                                    value={searchQuery}
+                                                    onChange={onSearchChangeHandler} 
+                                                    placeholder={`Find in ${filterName}`} 
+                                            />
+                                            <button 
+                                                    className='TaskListTitle-add' 
+                                                    onClick={onAddTaskClickHandler} 
+                                                    onKeyPress={onAddTaskKeyPressedHandler}
+                                            >
+                                                Add Task
+                                            </button>
+                                    </>); 
 
     return (
         <div className={listTitleClass}>
-           <h3 className='TaskListTitle-name'>{filterName}</h3>
+           <h3 className='TaskListTitle-name'>
+               {filterName}
+            </h3>
            <div className='TaskListTitle-time'>
-                <span>{formatedDate[0]}</span>
-                <span>{formatedDate[1]}</span>  
+                <span>
+                    {formatedDate[0]}
+               </span>
+                <span>
+                    {formatedDate[1]}
+               </span>  
             </div>
            {restLayout} 
         </div>
