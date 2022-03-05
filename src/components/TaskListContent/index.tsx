4644798d27c83ccc96e2411a067382e175560b6f
SergZu <sergzu0@gmail.com>
2022-03-05 18:01:54 +0600

    TaskListContent component code refactoring

--- src/components/TaskListContent/index.tsx
+++ src/components/TaskListContent/index.tsx
@@ -8,11 +8,25 @@ interface categoriesMap {
 }
 
 
-const TaskListContent = function({ tasks, categories, currentFilter, editTask, deleteTask, setTaskChecked, showCompleted, searchQuery, currentDate } : TaskListContentProps) {
+const TaskListContent = function({ 
+                                    tasks, 
+                                    categories, 
+                                    currentFilter, 
+                                    editTask, 
+                                    deleteTask, 
+                                    setTaskChecked, 
+                                    showCompleted, 
+                                    searchQuery, 
+                                    currentDate 
+    } : TaskListContentProps) {
     const createTaskList = () => {
 
-        const categoriesMap : categoriesMap = {'Unmarked' : []};
-        categories.sort().forEach((item) => {
+        const categoriesMap : categoriesMap = {
+            'Unmarked' : []
+        };
+        categories
+                    .sort()
+                    .forEach((item) => {
             if (item === '') return;
             categoriesMap[item] = [];
         })
@@ -21,7 +35,8 @@ const TaskListContent = function({ tasks, categories, currentFilter, editTask, d
         
         const filteredTasks = tasks.filter((item) => {
 
-            const searchRegExp = new RegExp(`${searchQuery}`, 'gi');
+            const searchRegExp = new RegExp(`${searchQuery}`, 'gi'); 
+            
             if (!searchRegExp.test(item.text)) return false
             
             if (item.complete && !showCompleted) return false;
@@ -34,17 +49,21 @@ const TaskListContent = function({ tasks, categories, currentFilter, editTask, d
             let targetDate : null | Date = null;
             if (item.expiredDate !== '') targetDate = new Date(item.expiredDate);
                 else return false;
+            
             const now = new Date();
+            
             if (currentFilter === 'Today') return targetDate.getDate() === now.getDate();
             if (currentFilter === 'Tomorrow') return targetDate.getDate() === new Date(Number(now) + 86400000).getDate();
             if (currentFilter === 'Upcoming') return Number(now) < Number(targetDate);
         });
 
-        filteredTasks.sort( (a,b) => {
-            let numA = typeof(a.expiredDate) === 'string' ? 0 : a.expiredDate;
-            let numB = typeof(b.expiredDate) === 'string' ? 0 : b.expiredDate;
-            return numA - numB
-        } );
+        filteredTasks.sort( 
+                            (a,b) => {
+                                        let numA = typeof(a.expiredDate) === 'string' ? 0 : a.expiredDate;
+                                        let numB = typeof(b.expiredDate) === 'string' ? 0 : b.expiredDate;
+                                        return numA - numB
+                                    } 
+        );
 
         filteredTasks.forEach( (item) => {
             const catName = item.category === '' ? 'Unmarked' : item.category;
@@ -57,19 +76,35 @@ const TaskListContent = function({ tasks, categories, currentFilter, editTask, d
             const tasks = categoriesMap[item];
             if (tasks.length === 0) continue;
             const listElement = (
-            <li key={item} className='task-list-content-categories' >
-                <h3 className='tl-categories-title'>{item}</h3>
-                {tasks.map((item : taskType) => (
-                <Task key={item.id} setTaskChecked={setTaskChecked} editTask={editTask} 
-                deleteTask={deleteTask} taskData={item} currentDate={currentDate} />
-                ))}
-            </li>
-            );
+                                    <li 
+                                        key={item} 
+                                        className='task-list-content-categories' 
+                                    >
+                                    <h3 className='tl-categories-title'>
+                                        {item}
+                                    </h3>
+                                    {tasks.map((item : taskType) => (
+                                                                        <Task 
+                                                                                key={item.id} 
+                                                                                setTaskChecked={setTaskChecked} 
+                                                                                editTask={editTask} 
+                                                                                deleteTask={deleteTask} 
+                                                                                taskData={item} 
+                                                                                currentDate={currentDate} 
+                                                                          />
+                                    ))}
+                                    </li>
+                                );
             result.push(listElement); 
         }
 
-        return result.length === 0 ? (<span id='task-list__no-tasks'>There are no tasks here</span>) :
-                                    (<>{result}</>)
+        return result.length === 0 ? 
+                                    (<span id='task-list__no-tasks'>
+                                                                        There are no tasks here
+                                    </span>) :
+                                    (<>
+                                        {result}
+                                    </>)
     };
 
     
