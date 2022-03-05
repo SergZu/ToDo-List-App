875db9f94d75614a02f878427e99556ae38953e8
SergZu <sergzu0@gmail.com>
2022-03-05 17:50:06 +0600

    TaskList component code refactoring

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -6,31 +6,67 @@ import './styles.scss';
 
 
 
-const TaskList = function({ tasks, currentFilter, editTask, addTask, deleteTask, currentDate, searchQuery, makeQuery, setTaskChecked, showCompleted } : TaskListProps) {
+const TaskList = function({ 
+                            tasks, 
+                            currentFilter, 
+                            editTask, 
+                            addTask, 
+                            deleteTask, 
+                            currentDate, 
+                            searchQuery, 
+                            makeQuery, 
+                            setTaskChecked, 
+                            showCompleted 
+                        } : TaskListProps) {
+    
     const computeID = () => {
         if (tasks.length === 0) return '1';
         const sortedByIdTasks = [...tasks].sort((a,b) => Number(b.id) - Number(a.id));
         return `${Number(sortedByIdTasks[0].id) + 1}`
     };
+    
     const createCategoryList = () : string[] | [] => {
         if (tasks.length === 0) return [];
         let result : Set<string> = new Set();
         const tempTaskArray = [...tasks];
-        tempTaskArray.forEach( (item) => {result.add(item.category)} );
+        tempTaskArray.forEach( 
+                                (item) => {
+                                            result.add(item.category)
+                                });
         return [...result]
     }
+    
     const nextId = computeID();
     const categories = createCategoryList();
     const categoriesListForAutocomplete = categories.length > 0 ? 
-    (<datalist id="categories" hidden={true}>
-        {categories.map((item : string)=>(<option value={item} key={item} />))}
-    </datalist>) : null;
+                                                                    (<datalist id="categories" hidden={true}>
+                                                                        {
+                                                                            categories.map(
+                                                                                        (item : string)=>(<option value={item} key={item} />)
+                                                                         )}
+                                                                    </datalist>) : 
+                                                                    null;
     return (
         <main className='TaskList'>
-            <TaskListTitle currentFilter={currentFilter} currentDate={currentDate} searchQuery={searchQuery} 
-                makeQuery={makeQuery} addTask={addTask} nextId={nextId} />
-            <TaskListContent tasks={tasks} categories={categories} currentFilter={currentFilter} showCompleted={showCompleted}
-                editTask={editTask} deleteTask={deleteTask} setTaskChecked={setTaskChecked} searchQuery={searchQuery}  currentDate={currentDate} />
+            <TaskListTitle 
+                            currentFilter={currentFilter} 
+                            currentDate={currentDate} 
+                            searchQuery={searchQuery} 
+                            makeQuery={makeQuery} 
+                            addTask={addTask} 
+                            nextId={nextId} 
+             />
+            <TaskListContent 
+                                tasks={tasks} 
+                                categories={categories} 
+                                currentFilter={currentFilter} 
+                                showCompleted={showCompleted}
+                                editTask={editTask} 
+                                deleteTask={deleteTask} 
+                                setTaskChecked={setTaskChecked} 
+                                searchQuery={searchQuery}  
+                                currentDate={currentDate} 
+            />
             {categoriesListForAutocomplete}
         </main>
     )
