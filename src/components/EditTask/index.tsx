3618f516e503cd5a436b4e499f98a1a38676cf3d
SergZu <sergzu0@gmail.com>
2022-03-05 17:22:39 +0600

    Refactor EditTask

--- src/components/EditTask/index.tsx
+++ src/components/EditTask/index.tsx
@@ -12,7 +12,9 @@ const EditTask = function(props : EditTaskProps) {
      const onChangeTextHandler : React.ChangeEventHandler<HTMLTextAreaElement> = (evt) => {
         setTaskError(false); 
         const newData = {...taskData};
-        const taskText  = evt.currentTarget.value.length < 150 ? evt.currentTarget.value : `${evt.currentTarget.value.slice(0, 150)}...`;
+        const taskText  = evt.currentTarget.value.length < 150 ? 
+                                                                    evt.currentTarget.value : 
+                                                                    `${evt.currentTarget.value.slice(0, 150)}...`;
         newData.text = taskText;
         changeTaskData(newData);
     };
@@ -33,7 +35,9 @@ const EditTask = function(props : EditTaskProps) {
 
     const onChangeCategoryHandler : React.ChangeEventHandler<HTMLInputElement> = (evt) => {
         const newData = {...taskData};
-        const taskCategory = evt.currentTarget.value.length < 40 ? evt.currentTarget.value : `${evt.currentTarget.value.slice(0, 40)}...`
+        const taskCategory = evt.currentTarget.value.length < 40 ? 
+                                                                    evt.currentTarget.value : 
+                                                                    `${evt.currentTarget.value.slice(0, 40)}...`;
         newData.category = taskCategory; 
         changeTaskData(newData);
     };
@@ -60,28 +64,75 @@ const EditTask = function(props : EditTaskProps) {
         scheduleTask((item) => !item);
     };
 
-    const dataInputValue = taskData.expiredDate !== '' ? convertDate(taskData.expiredDate as number) : '';
+    const dataInputValue = taskData.expiredDate !== '' ? 
+                                                         convertDate(taskData.expiredDate as number) : 
+                                                         '';
     console.log(`Expired Date : ${taskData.expiredDate}`)
-    const categoryInputValue = taskData.category !== '' ? taskData.category : '';
-    const importantBtnClass = taskData.important ? 'taskEdit-btn__important highlight' : 'taskEdit-btn__important';
-    const taskTextareaClass = taskTextError ? 'taskEdit-text invalid' : 'taskEdit-text';
+    const categoryInputValue = taskData.category !== '' ? 
+                                                          taskData.category : 
+                                                          '';
+    const importantBtnClass = taskData.important ? 
+                                                   'taskEdit-btn__important highlight' : 
+                                                   'taskEdit-btn__important';
+    const taskTextareaClass = taskTextError ? 
+                                              'taskEdit-text invalid' : 
+                                              'taskEdit-text';
     const expiredDateLayout = scheduled ? 
         (<label className='taskEdit-expired'>Expired date : 
-            <input type='datetime-local' value={dataInputValue} onChange={onDateChangeHandler} min={convertDate(Number(currentDate))} />
+            <input 
+                    type='datetime-local' 
+                    value={dataInputValue} 
+                    onChange={onDateChangeHandler} 
+                    min={convertDate(Number(currentDate))} />
          </label>) 
-         : (<input type='button' className='taskEdit-btn__schedule' onClick={onScheduleTask} value='Schedule' />);
+         : (<input 
+                    type='button' 
+                    className='taskEdit-btn__schedule' 
+                    onClick={onScheduleTask} 
+                    value='Schedule' 
+           />);
 
     return (
         <form className='task-editable'>
-            <h3 className='taskEdit-mode'>{(!!props.addTask) ? 'Add task' : 'Edit task'}</h3>
-            <textarea className={taskTextareaClass} value={taskData.text} onChange={onChangeTextHandler} 
-            placeholder={taskTextError ? 'Please Enter Task Name' : 'Task text'} />
-            <input type='button' className={importantBtnClass} onClick={onImportantClickHandler} value='Important' />
-            {expiredDateLayout}
-            <input type='text' className='taskEdit-category' onChange={onChangeCategoryHandler} placeholder='Task category' 
-                value={categoryInputValue}  list='categories' />
-            <input type='button' className='taskEdit-btn__cancel' onClick={cancelTask} />
-            <input type='button' className='taskEdit-btn__apply' onClick={submitTask} />
+            <h3 className='taskEdit-mode'>
+                {
+                    (!!props.addTask) ? 
+                                    'Add task' : 
+                                    'Edit task'}
+            </h3>
+            <textarea 
+                        className={taskTextareaClass} 
+                        value={taskData.text} 
+                        onChange={onChangeTextHandler} 
+                        placeholder={taskTextError ? 
+                                                    'Please Enter Task Name' : 
+                                                    'Task text'} 
+                />
+            <input 
+                    type='button' 
+                    className={importantBtnClass} 
+                    onClick={onImportantClickHandler} 
+                    value='Important' 
+             />
+                {expiredDateLayout}
+            <input 
+                    type='text' 
+                    className='taskEdit-category' 
+                    onChange={onChangeCategoryHandler} 
+                    placeholder='Task category' 
+                    value={categoryInputValue}  
+                    list='categories' 
+             />
+            <input 
+                    type='button' 
+                    className='taskEdit-btn__cancel' 
+                    onClick={cancelTask} 
+            />
+            <input 
+                    type='button' 
+                    className='taskEdit-btn__apply' 
+                    onClick={submitTask} 
+            />
         </form>
     )
 }
